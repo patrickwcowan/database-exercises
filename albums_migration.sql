6cb7e8c9bec53d2f84bd4f24c0fb1e1b973d6fa9
Parick Cowan <patrick.w.cowan@gmail.com>
2019-04-12 05:55:38 -0500

    added files from Thursday

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,2 +1,12 @@
 USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
 
+CREATE TABLE albums(
+                     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                     artist VARCHAR(128) NOT NULL,
+                     album_name VARCHAR(128) NOT NULL,
+                     release_date INT,
+                     sales DECIMAL(9,5),
+                     genre VARCHAR(255),
+                     PRIMARY KEY (id)
+);
---
+++ aliases_exercises.sql
@@ -0,0 +1,4 @@
+
+
+select concat(emp_no,' - ',last_name,', ',first_name) AS full_name, birth_date as DOB
+from employees limit 10;
